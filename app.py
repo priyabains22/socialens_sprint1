bea633ff52e86931c64f4a3c59286ab86ad7a8fb
priyabains22 <priya22011998b@gmail.com>
2024-04-24 16:47:12 +1000

    Decsriptive_statistics
    * Added the table for descriptive statistics( contains important columns only)
    * Connected the descriptive_html to Descriptive.py
    * Visualize the normal distribution

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, jsonify, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from werkzeug.utils import secure_filename
 import os
+import json
 from datetime import datetime
+import pandas as pd
 # referencing an external py file
-from descriptive_statistics import analyze_file  
+from descriptive_statistics import analyze_file
+
+
 
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'  # Required to use flash messages
@@ -14,28 +19,73 @@ ALLOWED_EXTENSIONS = {'csv', 'xlsx', 'xls'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB upload limit
 
+# Set the path for the json folder
+JSON_FOLDER = 'json_objects'
+JSON_EXTENSIONS = {'json'}
+app.config['JSON_FOLDER'] = JSON_FOLDER
+os.makedirs(JSON_FOLDER, exist_ok=True)
+
 # Ensure the upload directory exists
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+def create_json_graph(file_path):
+    try:
+        stats = analyze_file(file_path)
+
+        if stats is None:
+            return {'error': 'Failed to generate statistics'}, None
+
+        if 'error' in stats:
+            return {'error': stats['error']}, None
+
+        def convert(obj):
+            if isinstance(obj, (int, float)):
+                return obj
+            elif isinstance(obj, list):
+                return [convert(item) for item in obj]
+            elif isinstance(obj, dict):
+                return {key: convert(value) for key, value in obj.items()}
+            return str(obj)
+
+        stats = convert(stats)
+
+        json_file_name = os.path.basename(file_path).replace('.', '_') + '.json'
+        json_file_path = os.path.join(app.config['JSON_FOLDER'], json_file_name)
+
+
+        with open(json_file_path, 'w') as json_file:
+            json.dump(stats, json_file, indent=4)
+
+        return stats, os.path.basename(json_file_path)
+
+
+    except Exception as e:
+        return {'error': str(e)}, None
+    
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    breadcrumbs = [("Home", "/")]
+    return render_template('index.html', breadcrumbs=breadcrumbs)
 
 @app.route('/data-upload', methods=['GET', 'POST'])
 def data_upload():
+    breadcrumbs = [("Home", "/"), ("Data Upload", "/data-upload")]
     if request.method == 'POST':
         file = request.files['dataFile']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             flash(f'😄 File {filename} uploaded successfully! Good on you!', 'success')
-        else:
-            flash('😵 File upload failed. I can only ingest .csv, .xlsx, .xls files, sorry!', 'danger')
-        return redirect(url_for('data_upload'))
-    return render_template('data_upload.html')
+    files = os.listdir(app.config['UPLOAD_FOLDER'])
+    files_info = [{
+        'name': file,
+        'size': f"{os.stat(os.path.join(app.config['UPLOAD_FOLDER'], file)).st_size / 1024:.2f} KB",
+        'upload_time': datetime.fromtimestamp(os.stat(os.path.join(app.config['UPLOAD_FOLDER'], file)).st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+    } for file in files]
+    return render_template('data_upload.html', breadcrumbs=breadcrumbs, files=files_info)
 
 @app.route('/datasets')
 def datasets():
@@ -50,27 +100,126 @@ def datasets():
         })
     return render_template('datasets.html', files=files)
 
+@app.route('/explore-data')
+def explore_data():
+    breadcrumbs = [("Home", "/"), ("Explore Data", "/explore-data")]
+    files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if allowed_file(f)]
+    return render_template('explore_data.html', files=files, breadcrumbs=breadcrumbs)
+
+@app.route('/analyze-data', methods=['POST'])
+def analyze_data():
+    selected_file = request.form.get('selectedFile')
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], selected_file)
+    if not os.path.exists(file_path):
+        flash("File does not exist.", 'danger')
+        return redirect(url_for('explore_data'))
+
+    breadcrumbs = [("Home", "/"), ("Explore Data", "/explore-data"), ("Analyze Data", "/analyze-data")]
+
+    try:
+        # Check the file extension and read accordingly
+        if selected_file.endswith(('.xlsx', '.xls')):
+            # Read all sheets; each sheet's DataFrame is stored in a dictionary
+            xls = pd.ExcelFile(file_path)
+            sheets = {sheet_name: xls.parse(sheet_name) for sheet_name in xls.sheet_names}
+        elif selected_file.endswith('.csv'):
+            df = pd.read_csv(file_path)
+            sheets = {'CSV': df}
+        else:
+            flash("Unsupported file format.", 'danger')
+            return redirect(url_for('explore_data'))
+
+        # Convert each DataFrame to HTML table
+        tables_html = {sheet_name: df.to_html(classes='table table-striped', index=False) for sheet_name, df in sheets.items()}
+
+        # Call create_json_graph to print JSON data to console
+        for sheet_name, df in sheets.items():
+            json_data = create_json_graph(file_path)  # Assuming you want to print JSON for each sheet
+            print(f"JSON data for {sheet_name}:")
+            print(json_data)
+
+    except Exception as e:
+        flash(f"Failed to read the file: {str(e)}", 'danger')
+        return redirect(url_for('explore_data'))
+
+    return render_template('analysis_results.html', tables_html=tables_html, filename=selected_file, sheet_names=list(tables_html.keys()), json_data=json_data, breadcrumbs=breadcrumbs)
+
+@app.route('/list_files', methods=['GET'])
+def list_files():
+    files = [f for f in os.listdir(app.config['JSON_FOLDER']) if f.endswith('.json')]
+    return jsonify(files)
+
+@app.route('/get_json_data/<filename>', methods=['GET'])
+def get_json_data(filename):
+    json_file_path = os.path.join(app.config['JSON_FOLDER'], filename)
+    
+    if not os.path.exists(json_file_path):
+        return jsonify({'error': 'File not found'}), 404
+    
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+        
+    return jsonify(data)
+
+
+
 @app.route('/descriptive-statistics')
 def descriptive_statistics():
+    breadcrumbs = [("Home", "/"), ("Descriptive Statistics", "/descriptive-statistics")]
     files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if allowed_file(f)]
-    return render_template('descriptive_statistics.html', files=files)
+    stats = {}
+    json_files = {}
+    
+    for file in files:
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file)
+        stats[file], json_files[file] = create_json_graph(file_path)
+
+        # Call create_json_graph to print JSON data to console
+        print(f"JSON data for {file}:")
+        print(stats[file])
+    
+    return render_template('descriptive_statistics.html', files=files, stats=stats, json_files=json_files, breadcrumbs=breadcrumbs)
+
+
+@app.route('/descriptive-statistics-viewer')
+def descriptive_statistics_viewer():
+    breadcrumbs = [("Home", "/"), ("Descriptive Statistics Viewer", "/descriptive-statistics-viewer")]
+    files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if f.endswith(('.xlsx', '.xls', '.csv'))]  # Filter to include only relevant file types
+    return render_template('descriptive_statistics_viewer.html', files=files, breadcrumbs=breadcrumbs)
 
 @app.route('/analyze/<filename>')
 def analyze(filename):
+    breadcrumbs = [("Home", "/"), ("Descriptive Statistics", "/descriptive-statistics"), ("View Statistics", "/")]
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     if not os.path.exists(file_path):
         flash("File does not exist.", 'danger')
         return redirect(url_for('descriptive_statistics'))
     results = analyze_file(file_path)
-    return render_template('view_statistics.html', filename=filename, results=results)
+    return render_template('view_statistics.html', filename=filename, results= results,breadcrumbs=breadcrumbs)
 
 @app.route('/network-visualiser')
 def network_visualiser():
-    return render_template('network_visualiser.html')
+    breadcrumbs = [("Home", "/"), ("Network Visualiser", "/network-visualiser")]
+    return render_template('network_visualiser.html', breadcrumbs=breadcrumbs)
+
+@app.route('/json_objects/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(app.config['JSON_FOLDER'], filename)
+
+
 
 @app.route('/report-generator')
 def report_generator():
-    return render_template('report_generator.html')
+    breadcrumbs = [("Home", "/"), ("Report Generator", "/report-generator")]
+    files = []
+    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        stats = os.stat(filepath)
+        files.append({
+            'name': filename,
+        })
+    return render_template('report_generator.html', breadcrumbs=breadcrumbs)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
